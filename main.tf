91d79ace8c7c16bdbd9265c6ecc26806707ce36c
Nick <5348556+nickcmiller@users.noreply.github.com>
2022-12-22 07:05:04 +0000

    Updated script to use templatefile and local variables

--- main.tf
+++ main.tf
@@ -1,15 +1,96 @@
 # -- root/providers.tf --
 
-resource "aws_instance" "ubuntu_node" {
-    instance_type = "t2.micro"
-    ami = "ami-0574da719dca65348"
-    tags = {
-        Name = "Docker Ubuntu Instance"
+locals{
+  image_name = "test-nginx"
+}
+
+resource "aws_iam_policy" "ecr_policy" {
+  name        = "ecr-policy"
+  path        = "/"
+  description = "Policy for pushing images to ECR"
+
+  policy =  jsonencode(
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Sid": "VisualEditor0",
+              "Effect": "Allow",
+              "Action": [
+                  "ecr:PutImageTagMutability",
+                  "ecr:StartImageScan",
+                  "ecr:DescribeImageReplicationStatus",
+                  "ecr:ListTagsForResource",
+                  "ecr:UploadLayerPart",
+                  "ecr:BatchDeleteImage",
+                  "ecr:CreatePullThroughCacheRule",
+                  "ecr:ListImages",
+                  "ecr:BatchGetRepositoryScanningConfiguration",
+                  "ecr:DeleteRepository",
+                  "ecr:GetRegistryScanningConfiguration",
+                  "ecr:CompleteLayerUpload",
+                  "ecr:TagResource",
+                  "ecr:DescribeRepositories",
+                  "ecr:BatchCheckLayerAvailability",
+                  "ecr:ReplicateImage",
+                  "ecr:GetLifecyclePolicy",
+                  "ecr:GetRegistryPolicy",
+                  "ecr:PutLifecyclePolicy",
+                  "ecr:DescribeImageScanFindings",
+                  "ecr:GetLifecyclePolicyPreview",
+                  "ecr:CreateRepository",
+                  "ecr:DescribeRegistry",
+                  "ecr:PutImageScanningConfiguration",
+                  "ecr:GetDownloadUrlForLayer",
+                  "ecr:DescribePullThroughCacheRules",
+                  "ecr:GetAuthorizationToken",
+                  "ecr:PutRegistryScanningConfiguration",
+                  "ecr:DeletePullThroughCacheRule",
+                  "ecr:DeleteLifecyclePolicy",
+                  "ecr:PutImage",
+                  "ecr:BatchImportUpstreamImage",
+                  "ecr:UntagResource",
+                  "ecr:BatchGetImage",
+                  "ecr:DescribeImages",
+                  "ecr:StartLifecyclePolicyPreview",
+                  "ecr:InitiateLayerUpload",
+                  "ecr:GetRepositoryPolicy",
+                  "ecr:PutReplicationConfiguration"
+              ],
+              "Resource": "*"
+          }
+      ]
     }
-    key_name = "LabKey"
-    user_data = file("${path.module}/userdata.sh")
-    security_groups = ["${aws_security_group.allow_ssh_http.name}"]
-    depends_on = [aws_security_group.allow_ssh_http]
+  )
+}
+
+resource "aws_iam_role" "assume_role" {
+  name = "assume-role"
+  assume_role_policy = jsonencode(
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "ec2.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "role-policy-attachment" {
+  role       = aws_iam_role.assume_role.name
+  policy_arn = aws_iam_policy.ecr_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ecr_profile" {
+  name = "ecr-profile"
+  role = aws_iam_role.assume_role.name
 }
 
 
@@ -34,6 +115,15 @@ resource "aws_security_group" "allow_ssh_http" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  
+   ingress {
+    description      = "HTTP access"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   egress {
     from_port        = 0
@@ -52,11 +142,25 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 }
 
-resource "aws_ecr_repository" "nginx_ecr" {
-  name                 = "nginx-ecr"
+resource "aws_ecr_repository" "test_nginx_ecr_repo" {
+  name                 = "${local.image_name}"
   image_tag_mutability = "MUTABLE"
-
+  force_delete = true
+  
   image_scanning_configuration {
-    scan_on_push = true
+    scan_on_push = false
   }
+}
+
+resource "aws_instance" "amazon_linux_node" {
+    instance_type = "t2.micro"
+    ami = "ami-0b5eea76982371e91"
+    tags = {
+        Name = "Docker Amazon Linux Instance"
+    }
+    key_name = "LabKey"
+    iam_instance_profile = aws_iam_instance_profile.ecr_profile.name
+    user_data = templatefile("${path.module}/userdata.sh", {image_name=local.image_name})
+    security_groups = ["${aws_security_group.allow_ssh_http.name}"]
+    depends_on = [aws_security_group.allow_ssh_http, aws_ecr_repository.test_nginx_ecr_repo]
 }
--- userdata.sh
+++ userdata.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
-apt-get update -y
-apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release -y
+#install Docker
+yum update -y
+yum install docker -y
 
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+#allow the ec2-user to run Docker commands without being root
+usermod -a -G docker ec2-user
+id ec2-user
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
-apt-get update -y
+#make the directory for Dockerfile and navigate to it
+mkdir ${image_name}-file
+cd ${image_name}-file
 
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+#create the Dockerfile
+cat << EOF >> Dockerfile 
+FROM nginx:latest
+
+#Create a file that tells you the create date of the container
+RUN echo "Container created on $(date +'%A, %m/%d/%Y')" > createdate.txt
+
+#Modify the index.html to also tell you the create date of the container
+RUN echo "<html> <h1>Welcome to the Container!</h1><p> Created on $(date +'%A, %m/%d/%Y') </p></html>" > /usr/share/nginx/html/index.html
+EOF
+
+#Build Docker Nginx container
+docker build -t ${image_name}:v1 . #--build-arg now=$(date +'%A, %m/%d/%Y')
+
+#Run Container
+docker run -it --rm -d -p 8080:80 --name ${image_name} ${image_name}:v1
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 235447109042.dkr.ecr.us-east-1.amazonaws.com
+
+docker tag ${image_name}:v1 235447109042.dkr.ecr.us-east-1.amazonaws.com/${image_name}:v1
+
+docker push 235447109042.dkr.ecr.us-east-1.amazonaws.com/${image_name}:v1
